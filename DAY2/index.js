ce5fa9e39db720f5e3f72f36c94d3e4ab5644a06
Rahulw07 <rahul.wagh21@vit.edu>
2023-08-05 18:52:56 +0530

    Initial commit

---
+++ DAY2/index.js
@@ -0,0 +1,14 @@
+// Given an integer n, return a counter function. 
+//This counter function initially returns n and then returns 1 more
+// than the previous value every subsequent time it is called (n, n + 1, n + 2, etc).
+
+var createCounter = function(n) {
+    // Return an anonymous function (a closure) that increments n by 1 each time it's called
+    return () => n++;
+}
+
+const counter = createCounter(10);
+console.log(counter());
+console.log(counter());
+console.log(counter());
+ 
